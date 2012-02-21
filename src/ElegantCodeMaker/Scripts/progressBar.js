eea14f50e0369ba8e6514da908c11facf7b3d3ad
vquaiato <vinicius.quaiato@gmail.com>
2012-02-21 00:34:29 -0200

    click do botao e codigo lindo estourando

--- src/ElegantCodeMaker/Scripts/progressBar.js
+++ src/ElegantCodeMaker/Scripts/progressBar.js
@@ -1,29 +1,30 @@
-﻿$("#callEngine").click(function () {
-    alert("teste");
-    $("#analysisPanel").css('display', 'inline');
+﻿$(document).ready(function () {
+    $("#callEngine").click(function () {
+        $("#analysisPanel").css('display', 'inline');
 
-    var progressbarMsgs = new Array();
-    progressbarMsgs[0] = "[Retrieving code...]";
-    progressbarMsgs[1] = "[Analyzing anti-patterns...]";
-    progressbarMsgs[2] = "[Check for code bad-smells...]";
-    progressbarMsgs[3] = "[Analyzing complex structures...]";
-    progressbarMsgs[4] = "[Generating elegant code, waiting...]";
-    var count = 0;
+        var progressbarMsgs = new Array();
+        progressbarMsgs[0] = "[Retrieving code...]";
+        progressbarMsgs[1] = "[Analyzing anti-patterns...]";
+        progressbarMsgs[2] = "[Check for code bad-smells...]";
+        progressbarMsgs[3] = "[Analyzing complex structures...]";
+        progressbarMsgs[4] = "[Generating elegant code, waiting...]";
+        var count = 0;
 
-    $(function() {
-        $("#progressbar").progressbar({ value: 0 });
-        setTimeout(updateProgress, 500);
+        $(function () {
+            $("#progressbar").progressbar({ value: 0 });
+            setTimeout(updateProgress, 500);
 
-        function updateProgress() {
-            var progress;
-            progress = $("#progressbar").progressbar("option", "value");
-            if (progress < 100) {
-                if ((progress % 3) == 0) $("#progressbarMsg").text(progressbarMsgs[count++]);
-                $("#progressbar").progressbar("option", "value", progress + 5);
-                setTimeout(updateProgress, 500);
-            } else {
-                $("#submitcode").click();
+            function updateProgress() {
+                var progress;
+                progress = $("#progressbar").progressbar("option", "value");
+                if (progress < 100) {
+                    if ((progress % 3) == 0) $("#progressbarMsg").text(progressbarMsgs[count++]);
+                    $("#progressbar").progressbar("option", "value", progress + 5);
+                    setTimeout(updateProgress, 500);
+                } else {
+                    $("#submitcode").click();
+                }
             }
-        }
+        });
     });
 });
